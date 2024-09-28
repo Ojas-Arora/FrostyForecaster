45c1cd6733eba8e2551432f4f9a430395aac6b76
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-09-28 23:59:19 +0530

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,22 @@ from sklearn.ensemble import RandomForestClassifier
 
 st.set_page_config(page_title="Penguin Predictor", page_icon=":penguin:", layout="wide")
 
-# Custom CSS to change sidebar and main page background color
+# Custom CSS to change sidebar color
 st.markdown(
     """
     <style>
-    body {
-        background-color: #00CED1; /* Dark Turquoise for the main page */
-    }
-    .sidebar .sidebar-content {
-        background-color: #00CED1; /* Dark Turquoise for the sidebar */
+    .body {
+        background-color: #00CED1; /* Dark Turquoise */
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
+
 # Page Title
 st.header(" 💻 Discovering Penguin Species with Machine Learning")
+
 
 # Data Section
 with st.expander('🗂️ Data'):
